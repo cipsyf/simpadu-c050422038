768186eca04e42b5e3ccc943fa7d115e33b8ac3e
cipsyf <syifanadya135@gmail.com>
2023-10-30 20:16:21 +0800

    login, register, logout yg dapat diakses

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,13 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::get('/', function() {
+    return view('pages.auth.auth-login', ['type_menu' => '']);
+});
 
-Route::get('/', function () {
-    //return view('welcome');
-    return view('pages.blank-page', ['type_menu' => '']);
+Route::middleware(['auth'])->group(function() {
+
+    Route::get('home', function () {
+        return view('pages.app.dashboard-simpadu', ['type_menu' => '']);
+    })->name('home');
 });
